08b1522fed55e4f3040d456feced70c88c7e4ba9
Damien DeZurik <damien2017@osilabs.com>
2017-04-15 09:43:55 -0500

    Routes

--- src/routes.php
+++ src/routes.php
@@ -9,15 +9,32 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+$app->put('/new/{name}', function ($request, $response, $args) {
+    $this->logger->info("namedrop '/new' route");
+
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("> Hello, $name");
+
+    $p = new People();
+    $p->setName($name);
+    $p->save();
+
+    $app->flash('error', $e->getMessage());
+    $app->redirect('/namedrop');
+
+#    return $this->renderer->render($response, 'namedrop.phtml', $args);
+});
+
 $app->get('/namedrop/[{name}]', function ($request, $response, $args) {
     $this->logger->info("namedrop '/namedrop' route");
 
     $name = $request->getAttribute('name');
     $response->getBody()->write("> Hello, $name");
 
-    $author = new People();
-    $author->setName($name);
-    $author->save();
+    $people = PeopleQuery::create()->find();
+    foreach($person as $people) {
+        echo $person->getFirstName();
+    }
 
     return $this->renderer->render($response, 'namedrop.phtml', $args);
 });
