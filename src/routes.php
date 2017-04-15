13f4fa59209f340ea9de2b75d8b9cf394688b666
Damien DeZurik <ddezurik@rivercitytrail.com>
2017-04-15 10:40:06 -0500

    Loop in template

--- src/routes.php
+++ src/routes.php
@@ -12,25 +12,19 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 $app->get('/new/{name}', function ($request, $response, $args) {
     $this->logger->info("namedrop '/new' route");
 
-//    $name = $request->getAttribute('name');
-//    $response->getBody()->write("> Hello, $name");
-
+    $name = $request->getAttribute('name');
     $p = new People();
     $p->setName($name);
     $p->save();
 
     return $response->withStatus(302)->withHeader('Location', '/namedrop/');
-
-#    return $this->renderer->render($response, 'namedrop.phtml', $args);
 });
 
 $app->get('/namedrop/', function ($request, $response, $args) {
-    $this->logger->info("namedrop '/namedrop' route");
-
     $people = PeopleQuery::create()->find();
     foreach($people as $person) {
         $response->getBody()->write("> Hello, " . $person->getName());
     }
 
-    return $this->renderer->render($response, 'namedrop.phtml', $args);
+    return $this->renderer->render($response, 'namedrop.phtml', $people);
 });
