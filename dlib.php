412031d4b9ec3390c0e45f79572e10ed9a30ce96
Damien DeZurik <damien2017@osilabs.com>
2017-04-15 22:02:04 -0500

    Displaying existing names

---
+++ dlib.php
@@ -0,0 +1,6 @@
+<?php
+
+function prerender($item) {
+
+    return $item;
+}
--- public/index.php
+++ public/index.php
@@ -15,6 +15,8 @@ if (PHP_SAPI == 'cli-server') {
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require __DIR__ . '/dlib.php';
+
 session_start();
 
 // Instantiate the app
