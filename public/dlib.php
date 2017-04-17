6803e68de9020b487c3d004ddfa528efdb8ee16a
Damien DeZurik <ddezurik@rivercitytrail.com>
2017-04-16 21:01:29 -0500

    Displaying existing names

--- public/dlib.php
+++ public/dlib.php
@@ -23,7 +23,7 @@ function validateName( $name ) {
         $message[] = "Too long: '$name'.";
     }
 
-    if( 1 !== preg_match('~[0-9]~', $name)) {
+    if( 1 !== preg_match('/[0-9]/g', $name)) {
         $message[] = "Name may not contain numbers: '$name'.";
     }
 
