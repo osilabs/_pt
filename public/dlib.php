8a350298d780f06b5155bdf209bc399986d08487
Damien DeZurik <ddezurik@rivercitytrail.com>
2017-04-16 21:23:39 -0500

    Displaying existing names

--- public/dlib.php
+++ public/dlib.php
@@ -8,25 +8,30 @@ function prerender($item) {
     return '<span style="color:#666"><em><strong>' . htmlentities($item) . '</strong></em></span>';
 }
 
+/**
+ * Validate name input
+ * @param $name
+ * @return string
+ */
 function validateName( $name ) {
+    // Holds error messages
     $messages = [];
 
+    // Check min, max length. Assert no numbers.
     if (sizeof($name) == 0) {
         $messages[] = "Name was not submitted";
     }
-
-    if (strlen($name) < 2) {
+    if (strlen($name) < 3) {
         $messages[] = "Too short: '$name'";
     }
-
     if (strlen($name) >= 15) {
         $messages[] = "Too long: '$name'";
     }
-
     if( 1 === preg_match('/[0-9]/', $name)) {
         $messages[] = "Name may not contain numbers: '$name'";
     }
 
+    // Return string of messages or empty string if no issues found.
     if (count($messages)) {
         return join(" ", $messages);
     } else {
